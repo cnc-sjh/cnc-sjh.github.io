5c4397e691d446602622cb8e7fb2e60eb7e67662
cnc-sjh <110434704+cnc-sjh@users.noreply.github.com>
2022-10-06 18:10:53 +0900

    Add files via upload

---
+++ js/about.88305860.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdown_app"]=self["webpackChunkdown_app"]||[]).push([[443],{7381:function(n,a,u){u.r(a),u.d(a,{default:function(){return d}});var t=u(3396);const e={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function c(n,a){return(0,t.wg)(),(0,t.iD)("div",e,s)}var o=u(89);const i={},p=(0,o.Z)(i,[["render",c]]);var d=p}}]);
+//# sourceMappingURL=about.88305860.js.map
